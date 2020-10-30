29db5d3f10eb69ea8c0045feffb42b3514750d67
Estelle-xqy <Estelle-xqy@example.com>
2020-10-30 21:51:30 +0800

    Lab1:(1)sleep

---
+++ user/sleep.c
@@ -0,0 +1,14 @@
+#include "user/user.h"
+#include "../kernel/types.h"
+#include "kernel/stat.h"
+
+int main(int argn, char *argv[]){
+	if(argn != 2){
+		printf("must 1 argument for sleep\n");
+        exit();
+	}
+    int sleepNum = atoi(argv[1]);
+	printf("(nothing happens for a little while)\n");
+	sleep(sleepNum);
+	exit();
+}
